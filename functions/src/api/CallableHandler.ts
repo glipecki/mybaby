d55ca97af354a6a0e62333d3577ceded26472b45
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2018-08-24 20:21:30 +0200

    remove functions

--- functions/src/api/CallableHandler.ts
+++
@@ -1,6 +0,0 @@
-import {CallableContext} from 'firebase-functions/lib/providers/https';
-
-export abstract class CallableHandler<Request, Result> {
-  abstract getName(): string;
-  abstract handle(data: Request, context: CallableContext): Result;
-}
--- functions/src/sleep/Sleep.ts
+++
@@ -1,3 +0,0 @@
-export interface Sleep {
-  id: string;
-}
